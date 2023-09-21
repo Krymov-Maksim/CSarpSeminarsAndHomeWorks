caecdef88bd4ddf260dd7d5ffef8c9b74a87b817
Max_Kr <m.krymov@mail.ru>
2023-09-21 14:39:07 +0300

    Квадрат числа работает

--- Task001_SquareOfNumber/Program.cs
+++ Task001_SquareOfNumber/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// НАпишите программу, которая на вход принимает число и выдает его квадрат (число).
+
+Console.WriteLine("Input a number: ");
+
+int num = Convert.ToInt32 (Console.ReadLine ()); // Convert.ToInt32  - переводит строчные буквы в формат числа
+
+int res = num * num;
+
+Console.WriteLine($"Square of number {num} is {res}");
+
