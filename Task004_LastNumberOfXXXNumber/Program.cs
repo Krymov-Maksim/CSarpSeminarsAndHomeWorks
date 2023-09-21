1c5f27682e8f602b6e3f4aac45a4145ee8cb0e82
Max_Kr <m.krymov@mail.ru>
2023-09-21 16:38:57 +0300

    Ex004_LastNumber

---
+++ Task004_LastNumberOfXXXNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход трехзначное число
+//и на выходе показывает последнюю цифру этого числа
+
+Console.WriteLine("Input 3-x number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number<0)  
+    number = number * (-1);
+
+if (number>100 && number<999)
+    Console.WriteLine($"Here number is {number%10}");
+else 
+Console.WriteLine("Number is not thirth char");
