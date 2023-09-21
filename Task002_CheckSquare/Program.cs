edd66fbd7ff9f14276c502f7f13b7b874d5a28de
Max_Kr <m.krymov@mail.ru>
2023-09-21 15:57:35 +0300

    Task002_CheckSquare

---
+++ Task002_CheckSquare/Program.cs
@@ -0,0 +1,16 @@
+﻿// НАпишите программу, которая на вход принимает два числа и проверят,
+// является ли первое число квадратом второго
+
+Console.WriteLine("Inpit first number: ");
+int num1 = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Inpit second number: ");
+int num2 = Convert.ToInt32 (Console.ReadLine());
+
+if(num2*num2 == num1)
+    {
+    Console.WriteLine($"{num1} is square of {num2}");
+    }
+else 
+    {
+    Console.WriteLine($"{num1} is not qsuare of {num2}");
+    }
