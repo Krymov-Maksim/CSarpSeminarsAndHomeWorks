1677dda33d837cf433e5445dd5e30f85bb657ec9
Max_Kr <m.krymov@mail.ru>
2023-09-21 16:17:25 +0300

    Ex003_AllNumberToScreen

---
+++ Task003_OutputNatNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿// НАпишите программу, которая на вход принимает одно число (N)?
+//а на выходе показывает все числа от 0 до N
+
+// Console.WriteLine("Input number: ");
+// int n = Convert.ToInt32 (Console.ReadLine());
+// int count = 0;
+// while(count<n){
+//     Console.WriteLine($"{count}");
+//     count++;
+// }
+
+
+// НАпишите программу, которая на вход принимает одно число (N),
+//а на выходе показывает все числа от -N до N
+
+Console.WriteLine("Input number: ");
+int n = Convert.ToInt32 (Console.ReadLine());
+int current = n*(-1);
+for(int i = current; i<=n; i++){
+    Console.Write($"{i}");
+    }
