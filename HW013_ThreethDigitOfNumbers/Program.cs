fc6f66b4273b5fe3f9fa19bc13f814c6362f862d
Max_Kr <m.krymov@mail.ru>
2023-09-22 10:17:37 +0300

    HW013

---
+++ HW013_ThreethDigitOfNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+
+
+void ThirthNum (int number) {                           //метод вывода второго цифры 3-х значного числа
+    int digit3 = number/100%10;
+    Console.WriteLine($"Thirth digit -> {digit3}");
+}
+
+Console.WriteLine("Input any number: ");                //ввод любого числа
+int number = Convert.ToInt32(Console.ReadLine());
+
+              
+if (number < 0) number = number * (-1);                 //убираем отричательность, чтобы не выводился ответ со знаком "-"
+
+if (number < 100 && number > -100)                      //проверка 3х-значности числа
+    Console.Write("-> третьей цифры нет");
+
+else ThirthNum(number);                                 //передача на исполнение методу SecondNum
+
+
