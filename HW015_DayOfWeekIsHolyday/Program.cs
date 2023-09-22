2d8a0f7506f82c35b9d24cba5ec0a1c386efd67a
Max_Kr <m.krymov@mail.ru>
2023-09-22 10:38:28 +0300

    HW015

---
+++ HW015_DayOfWeekIsHolyday/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+void WeekEnd (int day){                                 //метод определения выходного дня по номеру дня недели
+    if (day == 6 || day == 7)
+    Console.WriteLine("-> да");
+    else 
+    Console.WriteLine("-> нет");
+}
+
+Console.WriteLine("Help you with weekend days. Input the day of the week: ");
+int day = Convert.ToInt32(Console.ReadLine());          // вводим день недели цифрами
+
+if (day < 1 || day > 7)                                 // проверяем правильность ввода числа от 1 до 7
+    Console.WriteLine("Такого дня недели не существует");
+
+else 
+WeekEnd (day);    
